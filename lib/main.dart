e925cd1969d006b7d033e54bf3219804213e302b
sdn1998 <sudindev1998@gmail.com>
2019-06-26 12:06:47 +0530

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: "GoogleSans"
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,16 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +27,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Form(
+          child: ListView(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            children: <Widget>[
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  SizedBox(width: 10.0),
+                  Text('Login',
+                      style:
+                      TextStyle(fontSize: 50.0, fontFamily: 'GoogleSans')),
+                  SizedBox(width: 150.0),
+                  Hero(
+                    tag: 'sd',
+                    child: Image(
+                      image: AssetImage('images/logo.png'),
+                      width: 130.0,
+                      height: 130.0,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 50.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  filled: true,
+                  fillColor: Colors.white12,
+                  suffixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white12,
+                  suffixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Cancel',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'GoogleSans')),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    onPressed: () {},
+                  ),
+                  RaisedButton(
+                    child: Text('Sign In',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'GoogleSans')),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              FlatButton(
+                child: Text('Not a user? Click here'),
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                onPressed: () {},
+              ),
+            ],
+          )),
     );
   }
+
 }
